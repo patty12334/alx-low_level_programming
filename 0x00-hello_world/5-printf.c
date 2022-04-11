36ec3de694bb71cb9cd02eaad6f5604ad25665a8
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-04-11 03:56:59 -0700

    betty commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/**main - Entry point
-*My hello, printf task
-*Return 1 after program execution
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
 */
 int main(void)
 {
