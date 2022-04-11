0410deb687106c8b795bb155d5dec3582911e387
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-04-10 23:58:30 -0700

    low level commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 int main (void)
 {
 printf("Size of a char: %d bytes(s)\n",
-sizeof(char))
+sizeof(char));
 printf("Size of an int: %d byte(s)\n",
 sizeof(int));
 printf("size of a long int: %d byte(s)\n",
