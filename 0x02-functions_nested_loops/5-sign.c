d409def2b1f661ff7abdc497177669be226d2ae0
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-04-13 03:00:27 -0700

    modified comment using betty style

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,13 @@
-nclude "main.h"
+#include "main.h"
 
 /**
-
  * print_sign - main function
-
  *
-
  * @n: character to compare
-
  *
-
  * Return: 1 if positive, 0 if zero, -1 if negative
-
  *
-
  */
-
-
 
 int print_sign(int n)
 
