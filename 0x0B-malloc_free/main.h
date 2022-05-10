38c2869d0035e2fc259f067fba91da7c586d034d
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-05-09 19:33:31 -0700

    main.h file changed

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,25 +1,7 @@
-#include "main.h"
-#include <stdlib.h>
-
-/**
-* create_array - creates an array of chars,
-* and initializes it with a specific char
-* @size: size af the array
-* @c: character initialized to the array
-* Return: NULL if it fails
-*         Pointer - if successful
-*/
-
-char *create_array(unsigned int size, char c)
-{
-	char *par;
-	unsigned int i;
-
-	par = malloc(sizeof(c) * size);
-
-	if (!size || !par)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		par[i] = c;
-	return (par);
-}
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
