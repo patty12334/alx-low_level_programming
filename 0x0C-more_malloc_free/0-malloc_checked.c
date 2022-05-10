0a452e1617016efdd2d8fca67bef0f2c14460741
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-05-09 18:00:12 -0700

    malloc solutions

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,37 +1,21 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdlib.h>
-
-
 
 /**
 
- * malloc_checked - allocates mem
-
- * @b: param
-
- * Return: void pointer
-
- */
-
-
+* malloc_checked - allocates mem
+* @b: param
+* Return: void pointer
+*/
 
 void *malloc_checked(unsigned int b)
-
 {
-
 	void *i = malloc(b);
 
 	/*size of parameter alreay added from funct call*/
-
 	/*Type cast not needed*/
 
-
-
 	if (i == NULL)
-
 		exit(98);
-
 	return (i);
-
 }
