40fd53517aa3e463c7110152103521310698ce80
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-05-25 15:11:31 -0700

    betty check on list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -5,14 +5,14 @@
 #include <stdio.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+* Description: singly linked list node structure
+* for Holberton project
+*/
+
 typedef struct list_s
 {
 	char *str;
