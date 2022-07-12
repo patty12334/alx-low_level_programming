104c69c79eb98469f9e322adb1188bb31649b51f
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-07-12 01:40:30 -0700

    task 1-6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data (n)
+ * @head: head of the list
+ * Return: sum of the data
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int add;
+
+	add = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			add += head->n;
+			head = head->next;
+		}
+	}
+
+	return (add);
+}
+	
+
+
