c7ed160ac018c30e53c4420f0dcdd7f53910322b
ugwu patiencechy@gmail.com <ugwupatiencechy@gmail.com>
2022-07-13 12:14:50 -0700

    task 2

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,59 @@
+/**
+* add - sums two numbers
+* @a: first parameter
+* @b: second parameter
+* Return: integer
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* sub - subtract two numbers
+* @a: first parameter
+* @b: second parameter
+* Return: integer
+*/
+
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: integer
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * div - divide two numbers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: integer
+ */
+
+int div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * mod - remainder of  two numbers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: integer
+ */
+
+int mod(int a, int b)
+{
+return (a % b);
+}
