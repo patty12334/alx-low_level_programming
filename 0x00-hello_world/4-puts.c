5f1d51b67602787e52d4f3da250dad1c3c81e298
Patience Ugwu <ugwupatiencechy@gmail.com>
2023-05-21 15:34:03 -0700

    all tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The containing function of this programme
+ * Return: this gives 0 if sucessful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
